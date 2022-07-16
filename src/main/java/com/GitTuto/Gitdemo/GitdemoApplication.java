f981b38784fcfe3b5fc5fb85c8d97686d5a79078
vinod1705 <102240882+vinod1705@users.noreply.github.com>
2022-07-16 15:01:26 +0530

    GitPracThirdCommit added integer j field

--- src/main/java/com/GitTuto/Gitdemo/GitdemoApplication.java
+++ src/main/java/com/GitTuto/Gitdemo/GitdemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitdemoApplication {
 	static String countryName = "India";
-
+	static int j= 10;
+	static int i= 9;
 	static String stateName = "AP";
 	public static void main(String[] args) {
 		SpringApplication.run(GitdemoApplication.class, args);
